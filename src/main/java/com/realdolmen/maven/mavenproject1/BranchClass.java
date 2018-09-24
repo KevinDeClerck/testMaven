df894bb4ff96a03fb4894861a0096edf2e3abb99
obarojeremiah <34315822+obarojeremiah@users.noreply.github.com>
2018-09-24 13:29:50 +0200

    Merge origin/master
    Conflicts:
    	README.md

---
+++ src/main/java/com/realdolmen/maven/mavenproject1/BranchClass.java
@@ -0,0 +1,42 @@
+package com.realdolmen.maven.mavenproject1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LJEBG43
+ */
+public class BranchClass {
+    private String name;
+    private String version;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "BranchClass{" + "name=" + name + ", version=" + version + '}';
+    }
+    
+    
+    
+}
+
+
